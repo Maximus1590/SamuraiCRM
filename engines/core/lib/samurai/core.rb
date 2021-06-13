5070ebbe419066b39841f0b7648c6e8e1abbf16a
Nguyen Vinh Hien <maximusnguyen1590@gmail.com>
2021-06-13 13:19:04 +0700

    first commit

---
+++ engines/core/lib/samurai/core.rb
@@ -0,0 +1,12 @@
+require 'sass-rails'
+require 'bootstrap-sass'
+require 'autoprefixer-rails'
+require 'jquery-rails'
+# require 'jquery_ujs'
+require 'devise'
+require 'cancan'
+
+module Samurai
+  module Core
+  end
+end  
